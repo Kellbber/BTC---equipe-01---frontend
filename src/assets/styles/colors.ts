c8ccf23080f0b8c7c736024c759390a6998739d5
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-08-08 20:37:02 -0300

    feat(project): styled-components

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primaryColors: "#FFFFFF",
+  secundaryColors: "#2EA8DC",
+
+  backgroundColor: "#D9D9D9",
+
+  textColorBody: "#000000",
+};
---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,8 @@
+export const constants = {
+  fontFamily: "Poppins",
+  headingFontSize: "20px",
+  headingLineHeight: "140%",
+
+  bodyFontSize: "15px",
+  bodyLineHeight: "140%",
+};
