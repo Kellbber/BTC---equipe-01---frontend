cd0cddc37dae6bc5d3646da05725ba602f06dc38
Kellbber <kellbberbarcarolo@gmail.com>
2022-08-09 20:14:04 -0300

    feat(adminConfig): search style

--- src/pages/AdminConfig/index.tsx
+++ src/pages/AdminConfig/index.tsx
@@ -1,27 +1,22 @@
-import * as S from './style'
-import {BiLogOut} from 'react-icons/bi'
-import { useEffect, useState } from 'react';
+import * as S from "./style";
+import { BiLogOut } from "react-icons/bi";
+import { useEffect, useState } from "react";
 
 const AdminConfig = () => {
-
+  
   const [icon, setIcon] = useState<boolean>(false);
 
-
-
-  function changeIcon (){
-    if(icon){
-      setIcon(false)
-    }
-    else{
-      setIcon(true)
+  function changeIcon() {
+    if (icon) {
+      setIcon(false);
+    } else {
+      setIcon(true);
     }
   }
 
-
-
   return (
     <S.background>
-        <S.heading>
+      <S.heading>
         <S.iconConfig>
           <p>
             Coluna
@@ -30,24 +25,24 @@ const AdminConfig = () => {
           </p>
         </S.iconConfig>
         <S.logins>
-        <BiLogOut cursor="pointer" size={30}/>
+          <BiLogOut cursor="pointer" size={30} />
         </S.logins>
-        </S.heading>
-        <S.content>
-            <S.adminSearch>
-                <input type="text" placeholder='Digite aqui...'/>
-                <S.selectEntity icons={icon}>
-                <select name="entity" id="entity" onClick={changeIcon}>
-                <option >Instituição</option>
-                <option >Usuário</option>
-                <option >Aluno</option>
-                </select>
-                <button>Buscar</button>
-                </S.selectEntity>
-            </S.adminSearch>
-        </S.content>
+      </S.heading>
+      <S.content>
+        <S.adminSearch>
+          <input type="text" placeholder="Digite aqui..." />
+          <S.selectEntity icons={icon}>
+            <select name="entity" id="entity" onClick={changeIcon}>
+              <option>Instituição</option>
+              <option>Usuário</option>
+              <option>Aluno</option>
+            </select>
+            <button>Buscar</button>
+          </S.selectEntity>
+        </S.adminSearch>
+      </S.content>
     </S.background>
-  )
-}
+  );
+};
 
 export default AdminConfig;
