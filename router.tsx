d28686fde414234ba772b752b0c82d152ab2beed
Kellbber <kellbberbarcarolo@gmail.com>
2022-08-30 21:34:39 -0300

    feat(vercel): creat vercel.json

--- router.tsx
+++ router.tsx
@@ -19,26 +19,33 @@ import ConfigHistoric from "./src/pages/ConfigHistoric";
 const Router = () => {
   return (
     <Routes>
+      
       <Route path={RoutePath.START} element={<StartPage />} />
       <Route path={RoutePath.LOGIN} element={<Login />} />
-      <Route path={RoutePath.ADMINST} element={<AdminConfig />} />
-      <Route path={RoutePath.FORMUSER} element={<ConfigUser />} />
-      <Route path={RoutePath.USERUP} element={<ConfigUser update={true} />} />
-      <Route path={RoutePath.STUDENT} element={<Student />} />
-      <Route path={RoutePath.USERS} element={<Users />} />
+
       <Route path={RoutePath.DASHBOARD} element={<Admin />} />
+      
+      <Route path={RoutePath.ADMINST} element={<AdminConfig />} />
       <Route path={RoutePath.FORMINST} element={<ConfigInstitution />} />
       <Route path={RoutePath.INSTUP}element={<ConfigInstitution update={true} />}/>
-      <Route path={RoutePath.FORMSTUD} element={<ConfigStudent />} />
-      <Route path={RoutePath.STUDUP} element={<ConfigStudent update={true} />}/>
-      <Route path={RoutePath.CREATEUSER} element={<CreateUser />} />
       <Route path={RoutePath.DETAILSINST} element={<DetailsInstitution />} />
-      <Route path={RoutePath.DETAILSSTUD} element={<DetailsStudent />} />
+
+      <Route path={RoutePath.USERS} element={<Users />} />
+      <Route path={RoutePath.FORMUSER} element={<ConfigUser />} />
+      <Route path={RoutePath.USERUP} element={<ConfigUser update={true} />} />
+      <Route path={RoutePath.CREATEUSER} element={<CreateUser />} />
       <Route path={RoutePath.DETAILSUSER} element={<DetailsUser />} />
       <Route path={RoutePath.MAILUSER} element={<FormMailUser />} />
+
+      <Route path={RoutePath.STUDENT} element={<Student />} />
+      <Route path={RoutePath.FORMSTUD} element={<ConfigStudent />} />
+      <Route path={RoutePath.STUDUP} element={<ConfigStudent update={true} />}/>
+      <Route path={RoutePath.DETAILSSTUD} element={<DetailsStudent />} />
+
       <Route path={RoutePath.DETAILSHIST} element={<DetailsHistoric/>} />
       <Route path={RoutePath.FOLLOWUPS} element={<ConfigHistoric/>} />
-      <Route path={RoutePath.FOLLOWUPSUP} element={<ConfigHistoric update={true}/>} />
+      <Route path={RoutePath.FOLLOWUPSUP} element={<ConfigHistoric update={true}/>} />     
+
     </Routes>
   );
 };
